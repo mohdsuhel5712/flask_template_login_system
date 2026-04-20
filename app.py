41990bd9010086830bacf4d8bed900c3cab665ea
mohdsuhel5712 <mohdsuhail5712@gmail.com>
2026-04-21 00:35:55 +0530

    foueth

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def login():
                  return render_template('login.html', error="Invalid credentials") 
       return render_template('login.html')
       
-if __name__ =='__main__':
-      app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
             
